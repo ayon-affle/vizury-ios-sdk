dfdd1db77dda18b88955514040f1a2eb15efe8bd
Ayon Chowdhury <md.rajib.sarwar@gmail.com>
2020-04-21 16:30:40 +0600

    added frameworks

---
+++ Frameworks/VizuryEventLogger/Vizury_iOS_SDK.framework/Headers/Vizury_iOS_SDK.h
@@ -0,0 +1,19 @@
+//
+//  Vizury_iOS_SDK.h
+//  Vizury-iOS-SDK
+//
+//  Created by Chowdhury Md Rajib  Sarwar on 21/4/20.
+//  Copyright © 2020 Chowdhury Md Rajib  Sarwar. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Vizury_iOS_SDK.
+FOUNDATION_EXPORT double Vizury_iOS_SDKVersionNumber;
+
+//! Project version string for Vizury_iOS_SDK.
+FOUNDATION_EXPORT const unsigned char Vizury_iOS_SDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Vizury_iOS_SDK/PublicHeader.h>
+
+
